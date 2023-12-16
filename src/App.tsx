c6c0736440213cef7afe2f35a795c199a5d2e6d0
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-17 01:13:23 +0530

    Complete Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {CalculatorBody} from "./calculator/CalculatorBody.tsx";
 import {Footer} from "./footer/Footer.tsx";
+import {Header} from "./header/Header.tsx";
 
 function App() {
 
 
   return (
     <>
+        <Header />
         <CalculatorBody />
         <Footer />
     </>
---
+++ src/header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export function Header() {
+    return (
+        <>
+            <h1>Basic Calculator</h1>
+        </>
+    );
+}
